965f4b5a0b00ccdcff2a1f4ef66024bb811bd8f1
GuillaumeDgr <dangerguillaume@gmail.com>
2018-01-24 09:47:38 +0100

    Content firebase value

--- app/src/main/java/fr/wcs/wcstravel/MainActivity.java
+++ app/src/main/java/fr/wcs/wcstravel/MainActivity.java
@@ -2,12 +2,40 @@ package fr.wcs.wcstravel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabaseReference;
+    String TAG = "WCSTravel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textViewFirebaseContent = findViewById(R.id.textViewFirebaseContent);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference.child("students").child("GuillaumeDgr").child("hasContent")
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Boolean content = snapshot.getValue(Boolean.class);
+                textViewFirebaseContent.setText(String.valueOf(content));
+                Log.d(TAG, "hasContent [" + content + "]");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 }
